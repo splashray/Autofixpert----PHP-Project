7e737dfb63dd39624fabb877a7196f7717a6787d
Dominic <Domnick080@outlook.com>
2022-04-20 13:24:49 +0100

    Update sign-up.php

--- sign-up.php
+++ sign-up.php
@@ -45,7 +45,7 @@ $password = mysqli_real_escape_string($conn, $password);
     <meta name="keywords" content=" Admin , Responsive, Landing, Bootstrap, App, Template, Mobile, iOS, Android, apple, creative app">
     <meta name="author" content="CodedThemes">
     <!-- Favicon icon -->
-    <link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
+    <link rel="icon" type="image/x-icon" href="./images/THE AUTO FIXPERT.svg">
     <!-- Google font--><link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,800" rel="stylesheet">
     <!-- Required Fremwork -->
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap/css/bootstrap.min.css">
